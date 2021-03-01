2d8f39c991dccbe0ebf2487d827b6c902547da12
sn568 <francois.van.staden@deutsche-boerse.com>
2021-03-01 20:11:46 +0000

    init

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("test.ui5piper.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"test/ui5piper/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
